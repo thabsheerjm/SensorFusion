ac0dbbca8ee477ca420b05a6b6774ff3de5a43af
thabsheerjm <thabsheerjm1@gmail.com>
2025-03-13 23:19:32 -0400

    add run project executable

--- Lidar_Obstacle_Detection/src/run.cpp
+++ Lidar_Obstacle_Detection/src/run.cpp
@@ -5,7 +5,7 @@
 #include "utils/processPointClouds/processPointClouds.cpp"
 # include <vtkObject.h>
 #include <filesystem>
-#include "utils/render/environment.cpp"
+#include "utils/render/environment.h"
 
 void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer, ProcessPointClouds<pcl::PointXYZI>* pointProcessorI, const pcl::PointCloud<pcl::PointXYZI>::Ptr& inputCloud)
 {
@@ -19,21 +19,20 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer, ProcessPointCloud
     int clusterId = 0 ;
     std::vector<Color> colors = {Color(1,0,0), Color(0,1,1), Color(1,1,0)};
 
-    for (pcl::PointCloud<pcl::PointXYZI>::Ptr cluster : cloudClusters)
+    for (size_t i = 0; i < cloudClusters.size(); ++i)
     {
+        const auto& cluster = cloudClusters[i];
 
-        std::cout<<"Cluster" << std::to_string(clusterId) << "has a size of ";
+        std::cout << "Cluster " << i << " has a size of ";
         pointProcessorI->numPoints(cluster);
-        renderPointCloud(viewer, cluster, "obstacleCloud" + std::to_string(clusterId), colors[clusterId % colors.size()]); 
+
+        std::string cloudName = "obstacleCloud" + std::to_string(i);
+        renderPointCloud(viewer, cluster, cloudName, colors[i % colors.size()]);
 
         Box box = pointProcessorI->BoundingBox(cluster);
-        renderBox(viewer, box, clusterId); 
-
-        ++clusterId;
-
+        renderBox(viewer, box, i);
     }
 }
-
 
 
 int main (int argc, char** argv)
@@ -44,15 +43,14 @@ int main (int argc, char** argv)
     pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
     CameraAngle setAngle = FPS;
     initCamera(setAngle, viewer);
-    // simpleHighway(viewer); 
 
     ProcessPointClouds<pcl::PointXYZI>* pointProcessorI = new ProcessPointClouds<pcl::PointXYZI>();
-    std::vector<std::filesystem::path> stream = pointProcessorI->streamPcd("../src/sensors/data/pcd/data_1"); 
+    std::vector<std::filesystem::path> stream = pointProcessorI->streamPcd("../data/pcd/data_1"); 
     auto streamIterator = stream.begin(); 
     pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloudI;
 
- 
-    while (!viewer->wasStopped ())
+
+    while (!viewer->wasStopped ()) 
     {
         viewer->removeAllPointClouds(); 
         viewer->removeAllShapes(); 
