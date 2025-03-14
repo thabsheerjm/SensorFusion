21c6966cace4b46b4a3ab78636cd68d6c87b6219
thabsheerjm <thabsheerjm1@gmail.com>
2025-03-13 23:20:53 -0400

    add project dependencies - reused from udacity

---
+++ Lidar_Obstacle_Detection/src/utils/render/environment.h
@@ -0,0 +1,59 @@
+//Adapted From Udacity Sensor Fusion Nano Degree project
+// 
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+#include "render.h"
+
+std::vector<Car> initHighway(bool renderScene, pcl::visualization::PCLVisualizer::Ptr& viewer)
+{
+
+    Car egoCar( Vect3(0,0,0), Vect3(4,2,2), Color(0,1,0), "egoCar");
+    Car car1( Vect3(15,0,0), Vect3(4,2,2), Color(0,0,1), "car1");
+    Car car2( Vect3(8,-4,0), Vect3(4,2,2), Color(0,0,1), "car2");	
+    Car car3( Vect3(-12,4,0), Vect3(4,2,2), Color(0,0,1), "car3");
+  
+    std::vector<Car> cars;
+    cars.push_back(egoCar);
+    cars.push_back(car1);
+    cars.push_back(car2);
+    cars.push_back(car3);
+
+    if(renderScene)
+    {
+        renderHighway(viewer);
+        egoCar.render(viewer);
+        car1.render(viewer);
+        car2.render(viewer);
+        car3.render(viewer);
+    }
+
+    return cars;
+}  
+
+
+//setAngle: SWITCH CAMERA ANGLE {XY, TopDown, Side, FPS}
+void initCamera(CameraAngle setAngle, pcl::visualization::PCLVisualizer::Ptr& viewer)
+{
+
+    viewer->setBackgroundColor (0, 0, 0);
+    
+    // set camera position and angle
+    viewer->initCameraParameters();
+    // distance away in meters
+    int distance = 16;
+    
+    switch(setAngle)
+    {
+        case XY : viewer->setCameraPosition(-distance, -distance, distance, 1, 1, 0); break;
+        case TopDown : viewer->setCameraPosition(0, 0, distance, 1, 0, 1); break;
+        case Side : viewer->setCameraPosition(0, -distance, 0, 0, 0, 1); break;
+        case FPS : viewer->setCameraPosition(-10, 0, 0, 0, 0, 1);
+    }
+
+    if(setAngle!=FPS)
+        viewer->addCoordinateSystem (1.0);
+}
+
+
+#endif
